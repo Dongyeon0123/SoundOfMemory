00a4366cd3348968ec532c8f3eef441feb7ffe53
Dongyeon0123 <165901822+Dongyeon0123@users.noreply.github.com>
2025-09-24 03:57:17 +0900

    아이콘 높이 조절

--- components/home/HeaderBar.tsx
+++ components/home/HeaderBar.tsx
@@ -5,7 +5,7 @@ import { FiSearch, FiSettings } from 'react-icons/fi';
 import { IoNotificationsOutline, IoPersonOutline } from 'react-icons/io5';
 import Link from 'next/link';
 
-const ICON_SIZE = 20;
+const ICON_SIZE = 22;
 
 type HeaderBarProps = {
   userId: string | null;
@@ -47,45 +47,46 @@ const HeaderBar: React.FC<HeaderBarProps> = ({
           }}
         >
           <div className={styles.icon}>
-            <FiSearch
-              size={ICON_SIZE}
-              color="#222"
-              style={{ cursor: 'pointer' }}
-              onClick={onSearchClick}
-            />
-            <Link href="/notifications" style={{ position: 'relative' }}>
-              <IoNotificationsOutline size={ICON_SIZE} color="#222" style={{ cursor: 'pointer' }} />
-              {unreadAnnouncementsCount > 0 && (
-                <div className={styles.plusBadge}>
-                  <span style={{ marginTop: 1 }}>
-                    {unreadAnnouncementsCount > 9 ? '9+' : unreadAnnouncementsCount}
-                  </span>
-                </div>
-              )}
+            <div className={styles.headerIconBtn}>
+              <FiSearch
+                size={ICON_SIZE}
+                color="#222"
+                style={{ cursor: 'pointer' }}
+                onClick={onSearchClick}
+              />
+            </div>
+            <Link href="/notifications" className={styles.headerIconBtn}>
+              <>
+                <IoNotificationsOutline size={ICON_SIZE} color="#222" style={{ cursor: 'pointer' }} />
+                {unreadAnnouncementsCount > 0 && (
+                  <div className={styles.plusBadge}>
+                    <span style={{ marginTop: 1 }}>
+                      {unreadAnnouncementsCount > 9 ? '9+' : unreadAnnouncementsCount}
+                    </span>
+                  </div>
+                )}
+              </>
             </Link>
-            <Link
-              href="/friend/requests"
-              style={{
-                display: 'inline-flex',
-                alignItems: 'center',
-                position: 'relative',
-              }}
-            >
-              <IoPersonOutline size={ICON_SIZE} color="#222" style={{ cursor: 'pointer', marginBottom: 5 }} />
-              {pendingRequestsCount > 0 && (
-                <div className={styles.plusBadge}>
-                  <span style={{ marginTop: 1 }}>
-                    {pendingRequestsCount > 9 ? '9+' : pendingRequestsCount}
-                </span>
-                </div>
-              )}
+            <Link href="/friend/requests" className={styles.headerIconBtn}>
+              <>
+                <IoPersonOutline size={ICON_SIZE} color="#222" style={{ cursor: 'pointer' }} />
+                {pendingRequestsCount > 0 && (
+                  <div className={styles.plusBadge}>
+                    <span style={{ marginTop: 1 }}>
+                      {pendingRequestsCount > 9 ? '9+' : pendingRequestsCount}
+                    </span>
+                  </div>
+                )}
+              </>
             </Link>
-            <FiSettings 
-              size={ICON_SIZE} 
-              color="#222" 
-              style={{ cursor: 'pointer' }}
-              onClick={handleSettingsClick}
-            />
+            <div className={styles.headerIconBtn}>
+              <FiSettings 
+                size={ICON_SIZE} 
+                color="#222" 
+                style={{ cursor: 'pointer' }}
+                onClick={handleSettingsClick}
+              />
+            </div>
           </div>
 
           {/* 로그인 버튼만 표시 (로그아웃 버튼 제거) */}
